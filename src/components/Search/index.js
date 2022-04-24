97a8f319d15d4e9a10cd3ef022f04e22e80524f4
devchicala <delciofrancisco1@gmail.com>
2022-04-24 15:01:50 +0100

    feat: refactoring main page

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,20 +1,17 @@
-import { UserResult } from '../UserResult'
 
 export const Search = () => {
   return (
-    <div>
+    <div style={{ paddingTop: '150px' }}>
       <div className="d-flex justify-content-center">
-        <h1>GITHUB PROFILE</h1>
+        <h1 className="text-white">GITHUB PROFILE</h1>
       </div>
 
       <div className="d-flex justify-content-center">
-        <input type="text" placeholder="Search a username" />
+        <input type="text" style={{ width: '500px' }} placeholder="Search a username" />
         <button type="text" style={{ width: '100px' }}>
           Buscar
         </button>
       </div>
-      <br />
-      <UserResult />
     </div>
   )
 }
